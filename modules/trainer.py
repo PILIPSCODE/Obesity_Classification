75aa1d2de0aa15f2f602a9d989b662b267b2e5d8
Pilipus <Pilipus@gmail.com>
2025-07-05 09:44:52 +0700

    update

--- modules/trainer.py
+++ modules/trainer.py
@@ -1,9 +1,5 @@
-"""Training module"""
-
 import os
-
 import tensorflow as tf
-from keras.utils.vis_utils import plot_model
 import tensorflow_transform as tft
 
 from transform import (
@@ -15,13 +11,9 @@ from transform import (
 
 
 def get_model(show_summary=True):
-    """
-    This function defines a Keras model and returns the model as a
-    Keras object.
-    """
-
-    # one-hot categorical features
+    """Define Keras model."""
     input_features = []
+
     for key, dim in CATEGORICAL_FEATURES.items():
         input_features.append(
             tf.keras.Input(shape=(dim + 1,), name=transformed_name(key))
@@ -32,8 +24,8 @@ def get_model(show_summary=True):
             tf.keras.Input(shape=(1,), name=transformed_name(feature))
         )
 
-    concatenate = tf.keras.layers.concatenate(input_features)
-    deep = tf.keras.layers.Dense(256, activation="relu")(concatenate)
+    concat = tf.keras.layers.concatenate(input_features)
+    deep = tf.keras.layers.Dense(256, activation="relu")(concat)
     deep = tf.keras.layers.Dense(64, activation="relu")(deep)
     deep = tf.keras.layers.Dense(16, activation="relu")(deep)
 
@@ -54,44 +46,27 @@ def get_model(show_summary=True):
 
 
 def gzip_reader_fn(filenames):
-    """Loads compressed data"""
+    """Load compressed data."""
     return tf.data.TFRecordDataset(filenames, compression_type="GZIP")
 
 
-def get_serve_tf_examples_fn(model, tf_transform_output):
-    """Returns a function that parses a serialized tf.Example."""
+def get_serve_json_fn(model, tf_transform_output):
+    """Return serving function that accepts JSON dict directly."""
 
     model.tft_layer = tf_transform_output.transform_features_layer()
 
     @tf.function
-    def serve_tf_examples_fn(serialized_tf_examples):
-        """Returns the output to be used in the serving signature."""
-        feature_spec = tf_transform_output.raw_feature_spec()
-        feature_spec.pop(LABEL_KEY)
-        parsed_features = tf.io.parse_example(serialized_tf_examples, feature_spec)
-
-        transformed_features = model.tft_layer(parsed_features)
-
+    def serve_json_fn(inputs):
+        transformed_features = model.tft_layer(inputs)
         outputs = model(transformed_features)
         return {"outputs": outputs}
 
-    return serve_tf_examples_fn
+    return serve_json_fn
 
 
 def input_fn(file_pattern, tf_transform_output, batch_size=64):
-    """Generates features and labels for tuning/training.
-    Args:
-        file_pattern: input tfrecord file pattern.
-        tf_transform_output: A TFTransformOutput.
-        batch_size: representing the number of consecutive elements of
-        returned dataset to combine in a single batch
-    Returns:
-        A dataset that contains (features, indices) tuple where features
-        is a dictionary of Tensors, and indices is a single Tensor of
-        label indices.
-    """
+    """Input fn for training and eval."""
     transformed_feature_spec = tf_transform_output.transformed_feature_spec().copy()
-
     label_key = transformed_name(LABEL_KEY)
 
     dataset = tf.data.experimental.make_batched_features_dataset(
@@ -107,12 +82,9 @@ def input_fn(file_pattern, tf_transform_output, batch_size=64):
     return dataset
 
 
-# TFX Trainer will call this function.
 def run_fn(fn_args):
-    """Train the model based on given args.
-    Args:
-    fn_args: Holds args used to train the model as name/value pairs.
-    """
+    """Train the model."""
+
     tf_transform_output = tft.TFTransformOutput(fn_args.transform_output)
 
     train_dataset = input_fn(fn_args.train_files, tf_transform_output, 64)
@@ -135,14 +107,17 @@ def run_fn(fn_args):
     )
 
     signatures = {
-        "serving_default": get_serve_tf_examples_fn(
+        "serving_default": get_serve_json_fn(
             model, tf_transform_output
         ).get_concrete_function(
-            tf.TensorSpec(shape=[None], dtype=tf.string, name="examples")
-        ),
+            {
+                "Height": tf.TensorSpec([None, 1], dtype=tf.float32),
+                "Weight": tf.TensorSpec([None, 1], dtype=tf.float32),
+                "BMI": tf.TensorSpec([None, 1], dtype=tf.float32),
+                "Age": tf.TensorSpec([None, 1], dtype=tf.float32),
+                "Gender": tf.TensorSpec([None, 1], dtype=tf.string),
+            }
+        )
     }
-    model.save(fn_args.serving_model_dir, save_format="tf", signatures=signatures)
 
-    plot_model(
-        model, to_file="images/model_plot.png", show_shapes=True, show_layer_names=True
-    )
+    model.save(fn_args.serving_model_dir, save_format="tf", signatures=signatures)
